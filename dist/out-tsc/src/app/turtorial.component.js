0a294f8e854219941c89e6a508ba34ffece33164
Nguyen Van Quang <39783340+ahhungknow@users.noreply.github.com>
2019-03-25 14:34:07 +0700

    Done

--- dist/out-tsc/src/app/turtorial.component.js
+++ dist/out-tsc/src/app/turtorial.component.js
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 var TurtorialComponent = /** @class */ (function () {
     function TurtorialComponent() {
         this.hello = "Hello ahhungknow - Angular";
+        this.showLineIf = false;
+        this.color = "blue";
+        this.colors = ["red", "green", "blue"];
     }
     TurtorialComponent = tslib_1.__decorate([
         Component({
             selector: 'first',
-            template: "\n    <h2>{{hello}}</h2>\n    <input type=\"text\" [(ngModel)] = \"fname\" />\n    <input type=\"text\" [(ngModel)] = \"lname\" />\n    <br>\n    Full name: {{fname}} {{lname}}"
+            template: "\n    <h2>{{hello}}</h2>\n    <h3 *ngIf=\"showLineIf\">This is ngIf directive line</h3>\n    <div [ngSwitch]=\"color\">\n        <p *ngSwitchCase=\"'red'\">This line color is red</p>\n        <p *ngSwitchCase=\"'blue'\">This line color is blue</p>\n        <p *ngSwitchCase=\"'green'\">This line color is green</p>\n        <p *ngSwitchDefault>Invalid color</p>\n    </div>\n    <ul>\n        <li *ngFor=\"let i of colors\">{{i}}</li>\n    </ul>"
         })
     ], TurtorialComponent);
     return TurtorialComponent;
--- src/app/turtorial.component.ts
+++ src/app/turtorial.component.ts
@@ -3,11 +3,20 @@ import{Component} from '@angular/core';
     selector:'first',
     template:`
     <h2>{{hello}}</h2>
-    <input type="text" [(ngModel)] = "fname" />
-    <input type="text" [(ngModel)] = "lname" />
-    <br>
-    Full name: {{fname}} {{lname}}`
+    <h3 *ngIf="showLineIf">This is ngIf directive line</h3>
+    <div [ngSwitch]="color">
+        <p *ngSwitchCase="'red'">This line color is red</p>
+        <p *ngSwitchCase="'blue'">This line color is blue</p>
+        <p *ngSwitchCase="'green'">This line color is green</p>
+        <p *ngSwitchDefault>Invalid color</p>
+    </div>
+    <ul>
+        <li *ngFor="let i of colors">{{i}}</li>
+    </ul>`
 })
 export class TurtorialComponent{
     public hello="Hello ahhungknow - Angular";
+    public showLineIf=false;
+    public color="blue";
+    public colors:string[] =["red","green","blue"];
 }
