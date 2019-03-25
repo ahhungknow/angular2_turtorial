072feecd97e7e13d0df80eae25a6d626aa8ddfe6
Nguyen Van Quang <39783340+ahhungknow@users.noreply.github.com>
2019-03-25 11:03:00 +0700

    no message

---
+++ dist/out-tsc/src/app/turtorial.component.js
@@ -0,0 +1,16 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var FirstComponent = /** @class */ (function () {
+    function FirstComponent() {
+    }
+    FirstComponent = tslib_1.__decorate([
+        Component({
+            selector: 'first',
+            template: "<h2>Hello ahhungknow - Angular</h2>\n    <h4>Style to Angular 2</h4>",
+            styles: ['h2 {color:red} h4{color:green}']
+        })
+    ], FirstComponent);
+    return FirstComponent;
+}());
+export { FirstComponent };
+//# sourceMappingURL=turtorial.component.js.map
